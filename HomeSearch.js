7ec33994035eb37cfbe767997a0615180dfb243f
AzaniaBG <azaniazbaker@gmail.com>
2020-02-04 15:35:00 -1000

    misc

--- HomeSearch.js
+++ HomeSearch.js
@@ -1,9 +1,17 @@
 'use strict'
 
 function handleSingleSearchSubmit() {
+    $("#js-single-movie-search-submit").on("click", event => {
+        event.preventDefault();
+        let singleSearchValue = $("#js-single-movie-search").val();
+        getOmdbMovieInfo(singleSearchValue, 10);
+        $("#js-single-movie-search").val("");
+        $("#multi-movie-search").hide();
+    })
 
 }
 
 function handleMultiSearchSubmit() {
+
     
 }
